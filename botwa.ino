76052eb995f074668170c4c58088d7ec81e42bb6
Tomi Mandala Putra <43200304+tomimandalap@users.noreply.github.com>
2021-01-03 01:17:52 +0700

    Add files via upload

---
+++ botwa.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <ThingESP.h>
+#include "DHT.h"
+
+DHT dht(2, DHT11);
+float t, h;
+
+ThingESP8266 thing("duino", "botnodemcu", "botnodemcu1234");
+
+int LED = LED_BUILTIN;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  dht.begin();
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, 1);
+
+  thing.SetWiFi("WiFi", "usbasp88");
+  thing.initDevice();
+}
+
+String HandleResponse(String query)
+{
+  //  dht11 
+  awal:
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    goto awal;
+  }
+  
+  if (query == "led on") {
+    digitalWrite(LED, 0);
+    return "Done: LED Turned ON";
+  }
+
+  else if (query == "led off") {
+    digitalWrite(LED, 1);
+    return "Done: LED Turned OFF";
+  }
+
+  else if (query == "led status")
+    return digitalRead(LED) ? "LED is OFF" : "LED is ON";
+
+  else if (query == "temp") {
+    return "Suhu: " + String(t,2) + "°C";
+  }
+
+  else if (query == "humd") {
+    return "Kelembaban: " + String(h,2) + "%";
+  }
+
+  else return "Your query was invalid..";
+}
+
+
+void loop()
+{
+
+  thing.Handle();
+
+}
